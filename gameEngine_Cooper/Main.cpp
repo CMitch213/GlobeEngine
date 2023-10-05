661553613b49b38a5f0f47364791185c8c72756d
CMitch <coopjmitch@outlook.com>
2023-10-05 17:43:05 -0400

    Created Basic Animations for the Entities

--- gameEngine_Cooper/Main.cpp
+++ gameEngine_Cooper/Main.cpp
@@ -12,24 +12,50 @@ int main() {
 	gameEngine.world = ECS::World::createWorld();
 
 	//Create entities
+	ECS::Entity* background;
+	ECS::Entity* tux;
 	ECS::Entity* stickFigure;
 
 	//Add systems to the engine
 	gameEngine.AddSystem(new RenderingSystem());
+	gameEngine.AddSystem(new AnimationsSystem());
 
-	//Create and assign 250 enitities to the world
-	for (int x = 0; x < 25; x++) {
-		for (int y = 0; y < 10; y++)
-		{
-			stickFigure = gameEngine.world->create();
+	//Create Entities
+	background = gameEngine.world->create();
+	tux = gameEngine.world->create();
+	stickFigure = gameEngine.world->create();
 
-			//Assign components to entities
-			stickFigure->assign<Transform>(x * 25 /*width of img*/, y * 32 /*height of img*/);
-			stickFigure->assign<Sprite2D>("../Debug/Pics/hero.png");
+	//Assign Componets to Entities
+	background->assign<Transform>(0, 0);
+	background->assign<Sprite2D>("../Debug/Pics/bg.jpg");
+	tux->assign<Transform>(50, 50);
+	tux->assign<Sprite2D>("../Debug/Pics/tux_from_linux.png");
+	tux->assign<Animator>(56, 72, 3, 9, 3000.0f);
+	tux->get<Animator>()->currentRow = 0;
+	stickFigure->assign<Transform>(100, 100);
+	stickFigure->assign<Sprite2D>("../Debug/Pics/herosheet.png");
+	stickFigure->assign<Animator>(32, 32, 4, 1, 2000.0f);
 
-			std::cout << "Entity ID: " << stickFigure->getEntityId() << std::endl;
-		}
-	}
+	//Check Which Entity They Are
+	std::cout << "EntityID: " << background->getEntityId() << std::endl;
+	std::cout << "EntityID: " << tux->getEntityId() << std::endl;
+	std::cout << "EntityID: " << stickFigure->getEntityId() << std::endl;
+
+	
+	////Create and assign 250 enitities to the world
+	//for (int x = 0; x < 25; x++) {
+	//	for (int y = 0; y < 10; y++)
+	//	{
+	//		stickFigure = gameEngine.world->create();
+
+	//		//Assign components to entities
+	//		stickFigure->assign<Transform>(x * 25 /*width of img*/, y * 32 /*height of img*/);
+	//		stickFigure->assign<Sprite2D>("../Debug/Pics/hero.png");
+
+	//		std::cout << "Entity ID: " << stickFigure->getEntityId() << std::endl;
+	//	}
+	//}
+	
 
 	//Pass Window Reference to Engine and Start
 	gameEngine.Start(&window);
--- gameEngine_Cooper/Systems/AnimationsSystem.cpp
+++ gameEngine_Cooper/Systems/AnimationsSystem.cpp
@@ -2,5 +2,19 @@
 
 void AnimationsSystem::tick(ECS::World* world, float deltaTime)
 {
+	world->each<Animator, Sprite2D>(
+		[&](ECS::Entity*, ECS::ComponentHandle<Animator> animator, ECS::ComponentHandle<Sprite2D> sprite)->void {
 
+			//By Default, Go to Next Picture.
+			animator->currentTime += deltaTime;
+			if (animator->currentTime >= animator->nextFrameTime) {
+				animator->currentTime = 0;
+				animator->currentColumn = (animator->currentColumn + 1) % animator->totalColumns;
+			}
+
+			sprite->picture.setTextureRect(sf::IntRect(animator->currentColumn * animator->spriteWidth,		/*For Left/Right Sides*/	
+				animator->currentRow * animator->spriteHeight	/*For Top and Bottom Sides*/,
+				animator->spriteWidth, animator->spriteHeight	/*For Width and Height*/ ));
+		}
+		);
 }
